fedc3f6328c4c1ef6689fec006170fc55b543952
Daniel <carreraprogrammer@gmail.com>
2023-12-05 10:44:06 -0500

    Add new files and update app.js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-const dbConnect = require('./db/dbConnect');
+const dbConnect = require('./config/dbConnect');
+
+const Category = require('./routes/category');
+const SubCategory = require('./routes/subCategory');
+const Product = require('./routes/product');
 
 dbConnect();
 
